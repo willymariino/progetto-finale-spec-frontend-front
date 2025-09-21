41a3ebfb7a2e469b25065353f3cedf0a0899e8ad
willymariino <wg.mariino@gmail.com>
2025-09-21 04:11:28 +0200

    feat: implement product detail fetching and loading state

--- src/pages/Product-detail.jsx
+++ src/pages/Product-detail.jsx
@@ -1,8 +1,49 @@
+import { useEffect, useState } from "react"
+import { useParams } from "react-router-dom"
+import axios from "axios"
+
+async function getProductDetail(id, setProduct, setLoading) {
+
+    try {
+        const res = await axios.get(`http://localhost:3001/products/${id}`)
+        setProduct(res.data.product)
+        setLoading(false)
+        console.log("oggetto completo", res.data)
+        console.log("solamente oggetto product", res.data.product)
+    }
+
+    catch (error) {
+        { console.error("errore nel caricamento dei dettagli del prodotto! 🙀🙀🙀🙀", error) }
+    }
+
+    finally {
+        console.log("operazione terminata")
+    }
+}
+
 function ProductDetail() {
+
+    const [product, setProduct] = useState(null)
+    const [loading, setLoading] = useState(true)
+    const { id } = useParams()
+
+
+    useEffect(() => {
+        getProductDetail(id, setProduct, setLoading)
+    }, [id])
+
+    if (loading) {
+        return (
+            <div>caricamento in corso...</div>
+        )
+    }
 
 
     return (
         <>
+
+            <h1>{product.title}</h1>
+
 
         </>
     )
